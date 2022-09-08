f9dae5ba522ef73cde2581f5d187f91402873790
saadhossain <mshsarkar@gmail.com>
2022-09-08 21:46:45 +0600

    If there is no item in the cart, hide the entire cart table

--- js/cart.js
+++ js/cart.js
@@ -2,7 +2,6 @@
 const addToCart = (cartBtn) =>{
     const productName = cartBtn.parentNode.parentNode.children[0].innerText;
     const price = cartBtn.parentNode.parentNode.children[2].children[0].innerText;
-    console.log(productName, price);
     //Get the cart if exist
     const cart = JSON.parse(localStorage.getItem('Cart'));
     if(!cart){
@@ -24,25 +23,35 @@ const addToCart = (cartBtn) =>{
         ];
         localStorage.setItem('Cart', JSON.stringify(cartItems));
     }
-    // console.log(cart);
     displayItems();
 }
 
 const displayItems = () =>{
+    //Get cart container
     const tableContainer = document.getElementById('table-container');
     tableContainer.innerHTML = '';
+    //Get the cart items from local storage
     const cartItems = JSON.parse(localStorage.getItem('Cart'));
     let serial = 0;
-    cartItems.forEach(product => {
-        serial++;
-        const tableRow = document.createElement('tr');
-        tableRow.innerHTML = `
-        <th>${serial}</th>
-        <td>${product.name}</td>
-        <td>$${product.price}</td>
-        `
-        tableContainer.appendChild(tableRow);
-    });
+    //Get the entire table
+    const cartTable = document.getElementById('cart-table');
+    //If there is no item in the cart, then completely hide the cart table
+    if(cartItems === null){
+        cartTable.classList.add('hidden');
+    }
+    else{
+        cartTable.classList.remove('hidden');
+        cartItems.forEach(product => {
+            serial++;
+            const tableRow = document.createElement('tr');
+            tableRow.innerHTML = `
+            <th>${serial}</th>
+            <td>${product.name}</td>
+            <td>$${product.price}</td>
+            `
+            tableContainer.appendChild(tableRow);
+        });
+    }
     //When We have array like object we should use reduct to find the sum of all price
     const totalPrice = cartItems.reduce((prev, next) => prev + parseInt(next.price), 0);
     document.getElementById('total-price').innerHTML = `$${totalPrice}`;
